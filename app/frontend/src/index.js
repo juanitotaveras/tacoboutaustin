ed0a1b3151462adc4e544a5c5e86ff15b60b6329
Caroline Shi <detectiveconan123456789@gmail.com>
2018-02-13 10:33:41 -0600

    Got navbar sort of working

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+// import Bootstrap from 'bootstrap/dist/css/bootstrap-theme.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
