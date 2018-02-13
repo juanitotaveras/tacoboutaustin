e46b960c5d2e6a5b2840076c006295da27156674
An Vo <thaiankh@gmail.com>
2018-02-13 17:19:43 -0600

    Try fixing the author unknown

--- app/backend/hotel_scraper.py
+++ app/backend/hotel_scraper.py
@@ -12,7 +12,7 @@ OMNI = "https://api.sygictravelapi.com/1.0/en/places/poi:698874"
 RESIDENCE = "https://api.sygictravelapi.com/1.0/en/places/poi:382553"
 
 
-# scrap 3 hotel and return it to a list
+# scrap 3 hotels and return them in a list
 def scrap_three_hotels():
 	response = requests.get(DRISKILL, headers = headers)
 	hotel1 = response.json()
