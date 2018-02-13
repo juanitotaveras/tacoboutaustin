b36c8fadcbcec5e372e0191dd1b3cf36a5cc565f
Caroline Shi <detectiveconan123456789@gmail.com>
2018-02-12 19:51:41 -0600

    Installed react bootstrap

--- app/frontend/src/Navbar.js
+++ app/frontend/src/Navbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavBar, Nav, NavItem } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
     return (
-      
+    	
+
+
     );
   }
 }
