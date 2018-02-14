87820760b07c3a29cfa9afd8102c76e8556532ef
Juanito Taveras <juanito.taveras@gmail.com>
2018-02-13 22:49:46 -0600

    Added everyone to About page

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -6,13 +6,20 @@ import Splash from './Splash';
 import About from './About';
 import { Route } from 'react-router-dom';
 import Router from './Router';
+import { Container, Row, Col } from 'reactstrap'
 
 
 class App extends Component {
   render() {
     return (
     	<div>
-        	<NavigationBar/>
+    	<Container>
+    	  <Row>
+    	    <Col>
+        	  <NavigationBar/>
+        	</Col>
+          </Row>
+    	</Container>
         	<Router />
         </div>
     );
--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
