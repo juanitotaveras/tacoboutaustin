4a155643fed2dafb36943738da9e3878f57dff69
Eduardo Campos <campos@wireless-10-148-88-75.public.utexas.edu>
2018-02-14 18:37:11 -0600

    Restaurant Cards info update

--- app/frontend/src/RestaurantCard.jsx
+++ app/frontend/src/RestaurantCard.jsx
@@ -12,8 +12,8 @@ export default class RestaurantCard extends Component {
           <CardImg top width="100%" src={this.props.image} alt="Card image cap" />
           <CardBody>
             <CardTitle>{this.props.name}</CardTitle>
-            <CardSubtitle>Type of food</CardSubtitle>
-            <CardText>Reviews, and google maps location</CardText>
+            <CardSubtitle>{this.props.food}</CardSubtitle>
+            <CardText>{this.props.rating}</CardText>
             <Button outline ><Link to={'/restaurants/' + this.props.id}>Check it out!</Link></Button>
           </CardBody>
         </Card>
@@ -24,5 +24,7 @@ export default class RestaurantCard extends Component {
 RestaurantCard.propTypes = {
     name: PropTypes.string,
     image: PropTypes.string,
-    id: PropTypes.string
+    id: PropTypes.string,
+    food: PropTypes.string,
+    rating: PropTypes.string
   }
--- app/frontend/src/Restaurants.jsx
+++ app/frontend/src/Restaurants.jsx
@@ -15,13 +15,31 @@ export default class Restaurants extends Component {
       		<Container>
       			<Row>
       				<Col>
-      					<RestaurantCard id="1" name="Franklin BBQ" image="https://static01.nyt.com/images/2017/03/15/dining/15REST-FRANKLIN-slide-HMM7/15REST-FRANKLIN-slide-HMM7-videoSixteenByNineJumbo1600.jpg"/>
+      					<RestaurantCard
+                id="1"
+                name="Franklin BBQ"
+                image="https://static01.nyt.com/images/2017/03/15/dining/15REST-FRANKLIN-slide-HMM7/15REST-FRANKLIN-slide-HMM7-videoSixteenByNineJumbo1600.jpg"
+                food="Barbecue Restaurant"
+                rating="★★★★★"
+                />
       				</Col>
       				<Col>
-      					<RestaurantCard id="2" name="The Oasis" image="https://laketravis.com/listify/wp-content/uploads/2016/10/oasis-10.jpg"/>
+      					<RestaurantCard
+                id="2"
+                name="The Oasis"
+                image="https://laketravis.com/listify/wp-content/uploads/2016/10/oasis-10.jpg"
+                food="Tex-Mex Restaurant"
+                rating="★★★★☆"
+                />
       				</Col>
       				<Col>
-      					<RestaurantCard id="3" name="Torchy's Tacos" image="https://www.papercitymag.com/wp-content/uploads/2017/04/56a66a7b2c26c.image_.jpg"/>
+      					<RestaurantCard
+                id="3"
+                name="Torchy's Tacos"
+                image="https://www.papercitymag.com/wp-content/uploads/2017/04/56a66a7b2c26c.image_.jpg"
+                food="Tacos"
+                rating="★★★☆☆"
+                />
       				</Col>
       			</Row>
       		</Container>
