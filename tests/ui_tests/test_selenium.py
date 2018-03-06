86c2276261ee52fbbfef5e73a7b0339ab7810f44
Juanito Taveras <juanito.taveras@gmail.com>
2018-03-06 11:20:11 -0600

    changed file locations

--- tests/ui_tests/test_selenium.py
+++ tests/ui_tests/test_selenium.py
@@ -1,8 +1,11 @@
+# dependencies required: 
+# pip install pyvirtualdisplay selenium  
+# apt-get install xvfb xserver-xephyr 
 from selenium import webdriver
 from pyvirtualdisplay import Display
 
 display = Display(visible=1, size=(1920, 1080))
 display.start()
-driver = webdriver.Chrome("/media/sf_git/tacoboutaustin/tests/chromedriver")
+driver = webdriver.Chrome("../chromedriver")
 driver.get('http://localhost:3000')
 print driver.title
