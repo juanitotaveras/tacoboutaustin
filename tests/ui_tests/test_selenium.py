bdbf44d3570ddf3224c26e218b9c2bb6f512632c
Juanito Taveras <juanito.taveras@gmail.com>
2018-03-08 09:49:48 -0600

    trying to get all unit tests to run

--- tests/ui_tests/test_selenium.py
+++ tests/ui_tests/test_selenium.py
@@ -3,9 +3,19 @@
 # apt-get install xvfb xserver-xephyr 
 from selenium import webdriver
 from pyvirtualdisplay import Display
+from unittest import main, TestCase
 
-display = Display(visible=0, size=(1920, 1080))
-display.start()
-driver = webdriver.Chrome("../chromedriver")
-driver.get('http://localhost:3000')
-print driver.title
+
+
+class UnitTest(TestCase):
+	def test1(self):
+		display = Display(visible=0, size=(1920, 1080))
+		display.start()
+		driver = webdriver.Chrome("../chromedriver")
+		driver.get('http://localhost:3000')
+
+		# Make sure that browser title is tacoboutaustin
+		print (driver.title)
+
+if __name__ == "__main__":
+	main()
