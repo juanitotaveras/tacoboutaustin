9427751d499c19d86c28bebe4a6a2556de215594
An Vo <thaiankh@gmail.com>
2018-03-18 21:51:47 -0500

    5th try: api test on Travis

--- tests/api_tests/api_tests.py
+++ tests/api_tests/api_tests.py
@@ -1,6 +1,6 @@
-import requests
 import sys
 import os
+import requests
 from unittest import main, TestCase
 
 FILE_ABSOLUTE_PATH = os.path.abspath(__file__)  # get absolute filepath
@@ -9,7 +9,7 @@ PARENT_DIR = os.path.dirname(CURRENT_DIR)  # get parent directory path
 BASE_DIR = os.path.dirname(PARENT_DIR)  # get grand parent directory path
 sys.path.append(BASE_DIR)
 
-api_url = "http://api.tacoboutaustin.me/"
+API_URL = "http://api.tacoboutaustin.me/"
 
 #from app.routes import *
 
@@ -17,56 +17,56 @@ api_url = "http://api.tacoboutaustin.me/"
 class TestApi(TestCase):
 
     def test_message_restaurant(self):
-        response = requests.get(api_url + "restaurants")
+        response = requests.get(API_URL + "restaurants")
         #print(response.json())
         status = response.json()['status']
         self.assertEqual(response.ok, True)
         self.assertEqual(status, "OK")
 
     def test_message_hotel(self):
-        response = requests.get(api_url + "hotels")
+        response = requests.get(API_URL + "hotels")
         status = response.json()['status']
         self.assertEqual(response.ok, True)
         self.assertEqual(status, "OK")
 
     def test_message_attraction(self):
-        response = requests.get(api_url + "attractions")
+        response = requests.get(API_URL + "attractions")
         status = response.json()['status']
         self.assertEqual(response.ok, True)
         self.assertEqual(status, "OK")
 
     def test_message_restaurant_id_1(self):
-        response = requests.get(api_url + "restaurants/0")
+        response = requests.get(API_URL + "restaurants/0")
         status = response.json()['status']
         self.assertEqual(response.ok, True)
         self.assertEqual(status, "OK")
-    
+
     def test_message_restaurant_id_2(self):
-        response = requests.get(api_url + "restaurants/201") # invalid id
+        response = requests.get(API_URL + "restaurants/201") # invalid id
         status = response.json()['status']
         self.assertEqual(response.ok, False)
         self.assertEqual(status, "INVALID_ID")
-    
+
     def test_message_hotel_id_1(self):
-        response = requests.get(api_url + "hotels/0")
+        response = requests.get(API_URL + "hotels/0")
         status = response.json()['status']
         self.assertEqual(response.ok, True)
         self.assertEqual(status, "OK")
 
     def test_message_hotel_id_2(self):
-        response = requests.get(api_url + "hotels/201")
+        response = requests.get(API_URL + "hotels/201")
         status = response.json()['status']
         self.assertEqual(response.ok, False)
         self.assertEqual(status, "INVALID_ID")
 
     def test_message_attraction_id_1(self):
-        response = requests.get(api_url + "attractions/1")
+        response = requests.get(API_URL + "attractions/1")
         status = response.json()['status']
         self.assertEqual(response.ok, True)
         self.assertEqual(status, "OK")
 
     def test_message_attraction_id_2(self):
-        response = requests.get(api_url + "attractions/201")
+        response = requests.get(API_URL + "attractions/201")
         status = response.json()['status']
         self.assertEqual(response.ok, False)
         self.assertEqual(status, "INVALID_ID")
@@ -74,5 +74,3 @@ class TestApi(TestCase):
 
 if __name__ == "__main__":
     main()
-
-
