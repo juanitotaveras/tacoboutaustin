25f851bfe87911ede786b7b6d181cc53817ee999
Juanito Taveras <juanito.taveras@gmail.com>
2018-03-20 09:53:46 -0500

    fixed test splash page

--- tests/ui_tests/test_splash_page.py
+++ tests/ui_tests/test_splash_page.py
@@ -11,6 +11,7 @@ class HomePageTest(TestCase):
 		self.display.start()
 		self.driver = webdriver.Chrome("../chromedriver")
 		self.driver.get('http://localhost:3000')
+		self.verificationErrors = []
 
 	def test_home_page_browser_tab_title(self):
 		# self.navigate_to_site()
