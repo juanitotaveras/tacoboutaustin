75e7e0b3074c35e04bf45459579c33537bdfa09c
root <root@ip-172-31-21-220.us-west-2.compute.internal>
2018-03-21 16:14:24 +0000

    some files

---
+++ tests/ui_tests/test_splash_page.py
@@ -0,0 +1,25 @@
+# dependencies required: 
+# pip install pyvirtualdisplay selenium  
+# apt-get install xvfb xserver-xephyr 
+from unittest import main, TestCase
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+class HomePageTest(TestCase):
+	def setUp(self):
+		self.display = Display(visible=0, size=(1920, 1080))
+		self.display.start()
+		self.driver = webdriver.Chrome("../chromedriver")
+		self.driver.get('http://localhost:3000')
+		self.verificationErrors = []
+
+	def test_home_page_browser_tab_title(self):
+		# self.navigate_to_site()
+		self.assertEqual(self.driver.title, "Tacoboutaustin")
+
+	def tearDown(self):
+		self.driver.quit()
+		self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+	main()
