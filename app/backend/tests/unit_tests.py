c31ccee170806d05077bc9f37613b0b11ddadfab
An Vo <thaiankh@gmail.com>
2018-03-22 10:43:19 -0500

    add more unit tests for close_by method

--- app/backend/tests/unit_tests.py
+++ app/backend/tests/unit_tests.py
@@ -22,7 +22,8 @@ sys.path.append(BASE_DIR)
 
 API_URL = "http://api.tacoboutaustin.me/"
 
-#from app.routes import *
+from main import *
+from tacoapi import *
 
 class TestApi(TestCase):
 
@@ -81,6 +82,42 @@ class TestApi(TestCase):
         status = response.json()['status']
         self.assertEqual(response.ok, False)
         self.assertEqual(status, "INVALID_ID")
+
+    def test_close_by_1(self):
+        places_data = close_places("restaurant", 2, 78704)
+        self.assertEqual(len(places_data), 2)
+        for place in places_data:
+            self.assertTrue(place, Restaurant)
+            detail = Restaurant.query.filter_by(id = place['id']).first()
+            self.assertEqual(detail.zipcode, 78704)
+
+    def test_close_by_2(self):
+        places_data = close_places("restaurant", 2, 79107)
+        self.assertEqual(len(places_data), 0)
+
+    def test_close_by_3(self):
+        places_data = close_places("hotel", 2, 78704)
+        self.assertEqual(len(places_data), 2)
+        for place in places_data:
+            self.assertTrue(place, Hotel)
+            detail = Hotel.query.filter_by(id = place['id']).first()
+            self.assertEqual(detail.zipcode, 78704)
+
+    def test_close_by_4(self):
+        places_data = close_places("hotel", 2, 79107)
+        self.assertEqual(len(places_data), 0)
+    
+    def test_close_by_5(self):
+        places_data = close_places("attraction", 2, 78704)
+        self.assertEqual(len(places_data), 2)
+        for place in places_data:
+            self.assertTrue(place, Attraction)
+            detail = Attraction.query.filter_by(id = place['id']).first()
+            self.assertEqual(detail.zipcode, 78704)
+
+    def test_close_by_6(self):
+        places_data = close_places("attraction", 2, 79107)
+        self.assertEqual(len(places_data), 0)
 
 
 if __name__ == "__main__":
