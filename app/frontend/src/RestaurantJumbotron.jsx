fc3b1e8d8319de8b7cb4eb93d760e59053940ec2
educampos96 <educampos96@gmail.com>
2018-03-27 09:24:10 -0500

    Add Carousel to restaurant jumbotron

--- app/frontend/src/RestaurantJumbotron.jsx
+++ app/frontend/src/RestaurantJumbotron.jsx
@@ -1,10 +1,81 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Jumbotron, Button, Row, Col } from 'reactstrap';
+import { 
+  Jumbotron, 
+  Button, 
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  Container } from 'reactstrap';
 
 var parsed_opening_hours;
 
+const styles = {
+  color:'red',
+  backgroundColor:'black',
+  fontWeight:'bold',
+  height: '400em'
+  };
+
+// temporary: delete
+const items = [
+  {
+    src: 'https://odis.homeaway.com/odis/destination/5f483ac3-763b-4506-9e15-3fff2e1e8291.hw1.jpg',
+    altText: 'Austin, Texas',
+    caption: 'Explore Austin!'
+  },
+  {
+    src: 'https://2u0yuyu2fs02fa819ut739hy-wpengine.netdna-ssl.com/wp-content/uploads/2017/05/crescent39.jpg',
+    altText: 'Postcard',
+    caption: 'Every tourist ever'
+  },
+  {
+    src: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/54444777/austin_taco_project_tacos.0.0.jpg',
+    altText: 'TACOZ',
+    caption: 'Our bread and butter'
+  }
+];
+
 export default class RestaurantJumbotron extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = { activeIndex: 0 };
+      this.next = this.next.bind(this);
+      this.previous = this.previous.bind(this);
+      this.goToIndex = this.goToIndex.bind(this);
+      this.onExiting = this.onExiting.bind(this);
+      this.onExited = this.onExited.bind(this);
+    }
+
+  onExiting() {
+      this.animating = true;
+  }
+
+  onExited() {
+      this.animating = false;
+  }
+
+  next() {
+      if (this.animating) return;
+      const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+      this.setState({ activeIndex: nextIndex });
+    }
+
+  previous() {
+      if (this.animating) return;
+      const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+      this.setState({ activeIndex: nextIndex });
+  }
+
+    goToIndex(newIndex) {
+      if (this.animating) return;
+      this.setState({ activeIndex: newIndex });
+  }
 
   parseBR() {
     var splitted_array = this.props.hours.split("<br>");
@@ -18,9 +89,27 @@ export default class RestaurantJumbotron extends Component {
   }
 
   render(props) {
+    const { activeIndex } = this.state;
+
+    const slides = this.props.images.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item}
+          >
+          <img top width="100%" height="400em" src={item} alt="Card image cap" />
+        </CarouselItem>
+      );
+    });
+
     var reviews = this.props.reviews.map(function(review){
       return <p>{review.text}<a href={review.link}>read more!</a></p>
     })
+
+    var more_images = this.props.images.map(function(image){
+                return <Col><img top width="100%" height="100em" src={image} alt="Card image cap" /></Col>;
+              })
 
     return (
     <div>
@@ -29,9 +118,24 @@ export default class RestaurantJumbotron extends Component {
         <p className="lead">Rating: {this.props.rating}/5<br/>
         </p>
           <Row>
-          <img top width="50%" height="400em" src={this.props.images[0]} alt="Card image cap" />
-          <iframe top width="50%" height="400em" frameborder="0" src={this.props.map_src} allowfullscreen>
+          <Col xs="6">
+          <div styles={styles}>
+            <Carousel
+              activeIndex={activeIndex}
+              next={this.next}
+              previous={this.previous}
+              >
+              {/*<CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />*/}
+              {slides}
+              <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+              <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+            </Carousel>
+            </div>
+            </Col>
+          <Col xs="6">
+            <iframe top width="100%" height="400em" frameborder="0" src={this.props.map_src} allowfullscreen>
           </iframe>
+          </Col>
         </Row>
         <p></p>
         <hr className="my-2" />
@@ -41,24 +145,7 @@ export default class RestaurantJumbotron extends Component {
         <blockquote><q>{reviews}</q></blockquote></p>
         <p><b>More pictures</b></p>
         <Row>
-          <Col>
-            <img top width="100%" height="100em" src={this.props.images[1]} alt="Card image cap" />
-          </Col>
-          <Col>
-            <img top width="100%" height="100em" src={this.props.images[2]} alt="Card image cap" />
-          </Col>
-          <Col>
-            <img top width="100%" height="100em" src={this.props.images[0]} alt="Card image cap" />
-          </Col>
-          <Col>
-            <img top width="100%" height="100em" src={this.props.images[0]} alt="Card image cap" />
-          </Col>
-          <Col>
-            <img top width="100%" height="100em" src={this.props.images[0]} alt="Card image cap" />
-          </Col>
-          <Col>
-            <img top width="100%" height="100em" src={this.props.images[0]} alt="Card image cap" />
-          </Col>
+          {more_images}
         </Row>
         <br/>
         {/*<p className="lead">
