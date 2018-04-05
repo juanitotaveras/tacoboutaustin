959c61a86ebaae31afdd030c5cd84f304ea0ced1
An Vo <thaiankh@gmail.com>
2018-04-05 16:52:49 -0500

    Better database with less duplicate code in models.py, open for more optimization next phase

--- app/backend/scrapers/fix_data.py
+++ app/backend/scrapers/fix_data.py
@@ -11,7 +11,7 @@
 from helper_methods import *
 
 def fix_sixth_street():
-    attraction = Attraction.query.filter_by(id='0').first()
+    attraction = Attraction.query.filter_by(name='Sixth street').first()
     attraction.address1 = "W 6th St"
     attraction.address2 = "Austin, TX 78703"
     attraction.zipcode = 78703
@@ -19,20 +19,20 @@ def fix_sixth_street():
 
 def fix_hotels():
     hotel = Hotel.query.filter_by(name ="Pecan Grove Rv Park").first()
-    hotel.images.image1 = "http://www.freedomintow.com/wp-content/uploads/2015/07/PecanGroveEntrance.jpg"
-    hotel.cover_image = "http://www.freedomintow.com/wp-content/uploads/2015/07/PecanGroveEntrance.jpg"
-    hotel.images.image2 = "http://www.freedomintow.com/wp-content/uploads/2015/07/PecanGroveDrive.jpg"
+    hotel.addImage("http://www.freedomintow.com/wp-content/uploads/2015/07/PecanGroveEntrance.jpg")
+    hotel.addCover("http://www.freedomintow.com/wp-content/uploads/2015/07/PecanGroveEntrance.jpg")
+    hotel.addImage("http://www.freedomintow.com/wp-content/uploads/2015/07/PecanGroveDrive.jpg")
     
     hotel = Hotel.query.filter_by(address1 = "109 E 7th St").first()
     hotel.name = "Aloft Austin Downtown"
     
     hotel = Hotel.query.filter_by(name = "Mehl's Motel").first()
-    hotel.images.image1 = "http://2.bp.blogspot.com/_YUD_TKP5xJk/TE3qHiCAGQI/AAAAAAAAADE/XNsebuQEf_Y/s1600/DSC08018.JPG"
-    hotel.cover_image = "http://2.bp.blogspot.com/_YUD_TKP5xJk/TE3qHiCAGQI/AAAAAAAAADE/XNsebuQEf_Y/s1600/DSC08018.JPG"
+    hotel.addImage("http://2.bp.blogspot.com/_YUD_TKP5xJk/TE3qHiCAGQI/AAAAAAAAADE/XNsebuQEf_Y/s1600/DSC08018.JPG")
+    hotel.addCover("http://2.bp.blogspot.com/_YUD_TKP5xJk/TE3qHiCAGQI/AAAAAAAAADE/XNsebuQEf_Y/s1600/DSC08018.JPG")
 
     hotel = Hotel.query.filter_by(name = "Austin Folk House").first()
-    hotel.images.image1 = "https://media.dexknows.com/media/photos/8532/b254/2c5d/a06e/9060/0d94/8eef/c653/image/8532b2542c5da06e90600d948eefc653.jpeg"
-    hotel.cover_image = "https://media.dexknows.com/media/photos/8532/b254/2c5d/a06e/9060/0d94/8eef/c653/image/8532b2542c5da06e90600d948eefc653.jpeg"
+    hotel.addImage("https://media.dexknows.com/media/photos/8532/b254/2c5d/a06e/9060/0d94/8eef/c653/image/8532b2542c5da06e90600d948eefc653.jpeg")
+    hotel.addCover("https://media.dexknows.com/media/photos/8532/b254/2c5d/a06e/9060/0d94/8eef/c653/image/8532b2542c5da06e90600d948eefc653.jpeg")
 
     hotel = Hotel.query.filter_by(name = "Studio 6 Austin").first()
     if hotel is not None:
@@ -116,7 +116,7 @@ def fix_zip_code():
 
 if __name__ == "__main__":
     #fix_sixth_street()
-    fix_zip_code()
+    #fix_zip_code()
     fix_hotels()
     fix_attractions()
     db.session.commit()
