0d818f3d62da33e4c6a1f6e672734e4b76cccb99
An Vo <thaiankh@gmail.com>
2018-04-12 14:26:07 -0500

    Continue refactor tacoapi.py

--- app/tacoapi.py
+++ app/tacoapi.py
@@ -20,6 +20,7 @@ numberOfClosePlace = 2
 
 dayDict = {"Sunday": 0, "Monday": 1, "Tuesday": 2,
            "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6}
+
 def getOtherType(type):
     if type == "restaurant":
         return "hotel", "attraction" 
@@ -79,7 +80,6 @@ def isBetween(open_hour, timeGet):
     close_time = int(close_timeH)*60 + int(close_timeM)
     if int(close_timeH) < 6:
         close_time += 1440
-    print(open_time, close_time, timeGet)
     if timeGet > open_time and timeGet < close_time:
         return True
     return False
@@ -90,7 +90,6 @@ def isOpen(hours, time):
     timeComp = time[1].split(":")
     timeGet = int(timeComp[0])*60 + int(timeComp[1])
     for open_hour in hours:
-        print(day, open_hour.day, open_hour.open_time, open_hour.close_time)
         if open_hour.day == day and isBetween(open_hour, timeGet):
             result = True
     return result
@@ -113,27 +112,14 @@ def getModel(type):
         return Attraction
     return Place
 
-
-# type: "restaurant", "hotel", "attraction"
-# args = requests.args
-def getList(args, type):
-    # get optional attributes
+def getSearchQuery(args, Model):
     search = args.get('search', default=None, type=str)
-    page = args.get('page', default=None, type=int)
-    order_by = args.get('order_by', default=None, type=str)
-    order = args.get('order', default=None, type=str)
     search_type = args.get('search_type', default=None, type=str)
-    rating = args.get('rating', default=None, type=str)
-    zipcode = request.args.get('zipcode', default=None, type=str)
-    categories = request.args.get('categories', default=None, type=str)
 
-    Model = getModel(type)  # get the right model
-
-    # process all the optional attributes
     if(search_type == 'and' or search is None):
-        query = Model.query.filter_by(type = type)
+        query = Model.query
     else:
-        query = Model.query.filter_by(type = type).filter_by(id=-1)
+        query = Model.query.filter_by(id=-1)
     if search is not None:
         searchTokens = search.split(',')
         for token in searchTokens:
@@ -141,12 +127,21 @@ def getList(args, type):
                 query = query.filter(or_(Model.zipcode.like(token), Model.name.like("%"+token+"%")))
             else:
                 query = Model.query.filter(or_(or_(Model.zipcode.like(token), Model.name.like("%"+token+"%"), Model.id.in_(place.id for place in query.all()))))
+    return query
+
+def getFilterQuery(query, args, Model):
+    rating = args.get('rating', default=None, type=str)
+    zipcode = request.args.get('zipcode', default=None, type=str)
+    categories = request.args.get('categories', default=None, type=str)
+
     if rating is not None:
         query = query.filter(Model.rating >= float(rating))
     if categories is not None:
         categoriesTokens = categories.split(',')
         for token in categoriesTokens:
             query = query.filter(Model.categories.any(category_id = token))
+    if zipcode is not None:
+        query = query.filter_by(zipcode=zipcode)
 
     if type == "restaurant":  # special case with restaurant, because restaurant have open hour and categories
         time = request.args.get('time', default=None, type=str)
@@ -158,10 +153,14 @@ def getList(args, type):
                 if isOpen(restaurant.hours, timeList):
                     open_restaurants.append(restaurant)
             query = query.filter(Restaurant.id.in_((rest.id for rest in open_restaurants)))
+    return query
 
 
-    if zipcode is not None:
-        query = query.filter_by(zipcode=zipcode)
+def getSortAndPageQuery(query, args, Model):
+    order_by = args.get('order_by', default=None, type=str)
+    order = args.get('order', default=None, type=str)
+    page = args.get('page', default=None, type=int)
+
     if order_by is None:
         order_by = 'name'
     if order is not None:
@@ -171,6 +170,18 @@ def getList(args, type):
             query = query.order_by(getQueryCol(Model, order_by).desc())
     if page is not None:
         query = query.limit(12).offset(12*(page-1))
+
+    return query
+
+
+# type: "restaurant", "hotel", "attraction"
+# args = requests.args
+def getList(args, type):
+    Model = getModel(type)  # get the right model
+
+    query = getSearchQuery(args, Model)             # Get search query
+    query = getFilterQuery(query, args, Model)      # Get all filter queries: rating, zipcode, categories, time
+    query = getSortAndPageQuery(query, args, Model) # Get sort and pagination queries
 
     # get the output of the request (a list of places with type provided)
     places = query.all()
@@ -193,62 +204,68 @@ def getList(args, type):
         output.append(place_data)
     return output
 
+def getOne(id, type):
+    Model = getModel(type)
+    place = Model.query.filter(getIdType(Model, type)).first()
+    if place == None:
+        response = jsonify({'status': "INVALID_ID"})
+        response.status_code = 404
+        return response
+
+    place_data = {}
+    place_data['id'] = getIdType(place, type)
+    place_data['name'] = place.name
+    place_data['phone'] = place.phone 
+    place_data['location'] = {
+        'lat': place.latitude, 'long': place.longtitude}
+    place_data['address'] = [place.address1, place.address2]
+    place_data['rating'] = place.rating
+    place_data['reviews'] = [{'text': place.reviews[0].text, 'link': place.reviews[0].link}, {
+        'text': place.reviews[1].text, 'link': place.reviews[1].link}, {
+        'text': place.reviews[2].text, 'link': place.reviews[2].link}]
+
+    image_data = []
+    for image in place.images:
+        image_data += [image.image_url]
+    place_data['images'] = image_data
+    if type == "restaurant":
+        hours_data = []
+        for hour in place.hours:
+            hour_data = {}
+            hour_data['day'] = hour.day
+            hour_data['open_time'] = hour.open_time
+            hour_data['close_time'] = hour.close_time
+            hours_data += [hour_data]
+        place_data['hours'] = hours_data
+ 
+    place_data['categories'] = []
+    for association in place.categories:
+        category_data = {}
+        category_data['id'] = association.category.id
+        category_data['name'] = association.category.name
+        place_data['categories'].append(category_data)
+
+    type1, type2 = getOtherType(type)
+    type1_place = close_places(place, type1, numberOfClosePlace)
+    type2_place = close_places(place, type2, numberOfClosePlace)
+    close_by_type1 = "close_by_" + type1 + "s"
+    close_by_type2 = "close_by_" + type2 + "s"
+    return place_data, close_by_type1, type1_place, close_by_type2, type2_place
+
 @app.route('/')
 def hello_user():
     return render_template('hello.html')
-
 
 @app.route('/restaurants')
 def get_restaurants():
     output = getList(request.args, "restaurant")
     return jsonify({'status': "OK", 'list': output, 'total': len(output)})
 
-
 @app.route('/restaurants/<id>')
 def get_restaurant(id):
-    restaurant = Restaurant.query.filter_by(restaurant_id=id).first()
-    if (restaurant == None):
-        response = jsonify({'status': "INVALID_ID"})
-        response.status_code = 404
-        return response
-
-    restaurant_data = {}
-    restaurant_data['id'] = restaurant.restaurant_id
-    restaurant_data['name'] = restaurant.name
-    restaurant_data['phone'] = restaurant.phone 
-    restaurant_data['location'] = {
-        'lat': restaurant.latitude, 'long': restaurant.longtitude}
-    restaurant_data['address'] = [restaurant.address1, restaurant.address2]
-    restaurant_data['rating'] = restaurant.rating
-    restaurant_data['reviews'] = [{'text': restaurant.reviews[0].text, 'link': restaurant.reviews[0].link}, {
-        'text': restaurant.reviews[1].text, 'link': restaurant.reviews[1].link}, {
-        'text': restaurant.reviews[2].text, 'link': restaurant.reviews[2].link}]
-
-    image_data = []
-    for image in restaurant.images:
-        image_data += [image.image_url]
-    restaurant_data['images'] = image_data
-
-    hours_data = []
-    for hour in restaurant.hours:
-        hour_data = {}
-        hour_data['day'] = hour.day
-        hour_data['open_time'] = hour.open_time
-        hour_data['close_time'] = hour.close_time
-        hours_data += [hour_data]
-    restaurant_data['hours'] = hours_data
- 
-    restaurant_data['categories'] = []
-    for association in restaurant.categories:
-        category_data = {}
-        category_data['id'] = association.category.id
-        category_data['name'] = association.category.name
-        restaurant_data['categories'].append(category_data)
-
-    hotels = close_places(restaurant, "hotel", numberOfClosePlace)
-    attractions = close_places(restaurant, "attraction", numberOfClosePlace)
-
-    return jsonify({'status': "OK", 'restaurant': restaurant_data, 'close_by_hotels': hotels, 'close_by_attractions': attractions})
+    restaurant_data, type1, type1_place, type2, type2_place = getOne(id, "restaurant")
+    
+    return jsonify({'status': "OK", 'retaurant': restaurant_data, type1: type1_place, type2: type2_place})
 
 
 @app.route('/hotels')
@@ -259,116 +276,17 @@ def get_hotels():
 
 @app.route('/hotels/<id>')
 def get_hotel(id):
-    hotel = Hotel.query.filter_by(hotel_id=id).first()
-    if (hotel == None):
-        response = jsonify({'status': "INVALID_ID"})
-        response.status_code = 404
-        return response
-
-    hotel_data = {}
-    hotel_data['id'] = hotel.hotel_id
-    hotel_data['name'] = hotel.name
-    hotel_data['phone'] = hotel.phone
-    hotel_data['location'] = {'lat': hotel.latitude, 'long': hotel.longtitude}
-    hotel_data['rating'] = hotel.rating
-    hotel_data['address'] = [hotel.address1, hotel.address2]
-    hotel_data['reviews'] = [{'text': hotel.reviews[0].text, 'link': hotel.reviews[0].link}, {
-        'text': hotel.reviews[1].text, 'link': hotel.reviews[1].link}, {
-        'text': hotel.reviews[2].text, 'link': hotel.reviews[2].link}]
-    image_data = []
-    for image in hotel.images:
-        image_data += [image.image_url]
-    hotel_data['images'] = image_data
-
-    hotel_data['categories'] = []
-    for association in hotel.categories:
-        category_data = {}
-        category_data['id'] = association.category.id
-        category_data['name'] = association.category.name
-        hotel_data['categories'].append(category_data)
-
-    restaurants = close_places(hotel, "restaurant", numberOfClosePlace)
-    attractions = close_places(hotel, "attraction", numberOfClosePlace)
-
-    return jsonify({'status': "OK", 'hotel': hotel_data, 'close_by_restaurants': restaurants, 'close_by_attractions': attractions})
-
+    hotel_data, type1, type1_place, type2, type2_place = getOne(id, "hotel")
+    
+    return jsonify({'status': "OK", 'retaurant': hotel_data, type1: type1_place, type2: type2_place})
 
 @app.route('/attractions')
 def get_attractions():
     output = getList(request.args, "attraction")
     return jsonify({'status': "OK", 'list': output, 'total': len(output)})
 
-
-
 @app.route('/attractions/<id>')
 def get_attraction(id):
-    attraction = Attraction.query.filter_by(attraction_id=id).first()
-    if (attraction == None):
-        response = jsonify({'status': "INVALID_ID"})
-        response.status_code = 404
-        return response
-
-    attraction_data = {}
-    attraction_data['id'] = attraction.attraction_id
-    attraction_data['name'] = attraction.name
-    attraction_data['phone'] = attraction.phone
-    attraction_data['location'] = {
-        'lat': attraction.latitude, 'long': attraction.longtitude}
-    attraction_data['rating'] = attraction.rating
-    attraction_data['address'] = [attraction.address1, attraction.address2]
-    attraction_data['reviews'] = [{'text': attraction.reviews[0].text, 'link': attraction.reviews[0].link}, {
-        'text': attraction.reviews[1].text, 'link': attraction.reviews[1].link}, {
-        'text': attraction.reviews[2].text, 'link': attraction.reviews[2].link}]
-    image_data = []
-    for image in attraction.images:
-        image_data += [image.image_url]
-    attraction_data['images'] = image_data
-    attraction_data['categories'] = []
-    for association in attraction.categories:
-        category_data = {}
-        category_data['id'] = association.category.id
-        category_data['name'] = association.category.name
-        attraction_data['categories'].append(category_data)
-
-    restaurants = close_places(attraction, "restaurant", numberOfClosePlace)
-    hotels = close_places(attraction, "hotel", numberOfClosePlace)
-
-    return jsonify({'status': "OK", 'attraction': attraction_data, 'close_by_restaurants': restaurants, 'close_by_hotels': hotels})
-
-@app.route('/categories')
-def get_categories():
-    type = request.args.get('type', default=None, type=str)
-    query_string = "SELECT category.*, COUNT(place.id) as Count FROM category inner join association on category.id = association.category_id inner join place on place.id = association.place_id"
-
-    if type is not None:
-        query_string = query_string + " WHERE place.type = \"" + type + "\""  
-    query_string = query_string + " GROUP BY category.id" 
-    query = text(query_string)
-    categories = db.session.execute(query)
-    output = []
-    for category in categories:
-        category_data = {}
-        category_data['value'] = category.id
-        category_data['label'] = category.name
-        category_data['number'] = category.Count
-        output.append(category_data)
-    return jsonify({'status': "OK", 'list': output, 'total': len(output)})
-
-@app.route('/zipcodes')
-def get_zipcodes():
-    type = request.args.get('type', default=None, type=str)
-    query_string = "SELECT zipcode.*, COUNT(place.id) as Count FROM zipcode inner join place on place.zipcode = zipcode.value"
-
-    if type is not None:
-        query_string = query_string + " WHERE place.type = \"" + type + "\""  
-    query_string = query_string + " GROUP BY zipcode.value"
-    query = text(query_string)
-    zipcodes = db.session.execute(query)
-
-    output = []
-    for zipcode in zipcodes:
-        zipcode_data = {}
-        zipcode_data['value'] = zipcode.value
-        zipcode_data['label'] = str(zipcode.value) + ": " + str(zipcode.Count) + " place(s)"
-        output.append(zipcode_data)
-    return jsonify({'status': "OK", 'list': output, 'total': len(output)})
+    attraction_data, type1, type1_place, type2, type2_place = getOne(id, "attraction")
+    
+    return jsonify({'status': "OK", 'retaurant': attraction_data, type1: type1_place, type2: type2_place})
