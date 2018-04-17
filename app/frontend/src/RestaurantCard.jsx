39f45b960a56efb1c70b1e4a73ff31047f77d5fe
educampos96 <educampos96@gmail.com>
2018-04-17 11:20:14 -0500

    Add gray taco rating to all cards

--- app/frontend/src/RestaurantCard.jsx
+++ app/frontend/src/RestaurantCard.jsx
@@ -5,13 +5,27 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
 import TacoFull from './assets/taco_full.png';
+import TacoHalf from './assets/taco_half.png';
+import TacoGray from './assets/taco_gray.png';
 
 export default class RestaurantCard extends Component {
   render(props) {
     var r = this.props.restaurant;
 
-    var taco_rating = [...Array(Math.floor(r.rating))].map(function(review) {
+    let full_tacos = Math.floor(r.rating)
+    let half_tacos = Math.ceil(r.rating) - full_tacos
+    let gray_tacos = 5 - full_tacos - half_tacos
+
+    var full_tacos_rating = [...Array(full_tacos)].map(function(idx) {
       return <img top width="20em" src={TacoFull}/>
+    })
+
+    var half_taco_rating = [...Array(half_tacos)].map(function(idx) {
+      return <img top width="20em" src={TacoHalf}/>
+    })
+
+    var gray_taco_rating = [...Array(gray_tacos)].map(function(idx) {
+      return <img top width="20em" src={TacoGray}/>
     })
     
     return (
@@ -21,7 +35,7 @@ export default class RestaurantCard extends Component {
           <CardBody>
             <CardTitle className="cardTitleText">{r.name}</CardTitle>
             <CardSubtitle>{r.food}</CardSubtitle>
-            <CardText className="cardSubText"><span>Rating: {taco_rating}</span> <span style={{float: 'right'}}>Zip code: {r.zip_code}</span></CardText>
+            <CardText className="cardSubText"><span>Rating: {full_tacos_rating}{half_taco_rating}{gray_taco_rating}</span> <span style={{float: 'right'}}>Zip code: {r.zip_code}</span></CardText>
             <Link to={'/restaurants/' + r.id}><Button outline >Check it out!</Button></Link>
           </CardBody>
         </Card>
